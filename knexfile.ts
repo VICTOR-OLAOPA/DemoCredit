80db03e674111bd4b6a343f5da27d6dfe4b98eab
VICTOR-OLAOPA <olaopavictor@gmail.com>
2024-06-19 13:17:21 +0100

    Update knexfile.ts - Changed client from sqlite3 to mysql2 - Updated dotenv variables and migrated database to new server

--- knexfile.ts
+++ knexfile.ts
@@ -1,47 +1,24 @@
-import type { Knex } from "knex";
+import { Knex } from 'knex';
+import dotenv from 'dotenv';
 
-// Update with your config settings.
+dotenv.config();
 
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: "sqlite3",
+    client: 'mysql2',
     connection: {
-      filename: "./dev.sqlite3"
-    }
-  },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     migrations: {
-      tableName: "knex_migrations"
-    }
+      directory: './src/migrations',
+    },
+    seeds: {
+      directory: './src/seeds',
+    },
   },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  }
-
 };
 
 module.exports = config;
