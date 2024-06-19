08941e2001d37928ebbec472e70a3d703421be3c
VICTOR-OLAOPA <olaopavictor@gmail.com>
2024-06-19 21:18:41 +0100

    Update app.ts - Edited the app.ts file. Included JSON response for the API's homepage, containing a brief introduction to the API, as well as directions.

--- src/app.ts
+++ src/app.ts
@@ -41,6 +41,26 @@ app.use(expressWinston.logger({
 
 app.use(express.json());
 
+
+
+app.get('/', (req, res) => {
+  res.json({
+    name: "DemoCredit API",
+    version: "1.0.0",
+    description: "Welcome to the DemoCredit API. This API allows you to manage users, wallets, and transactions for a micro-lending platform.",
+    endpoints: {
+      users: "/api/users",
+      wallets: "/api/wallets",
+      transactions: "/api/transactions"
+    },
+    documentation: "https://docs.google.com/document/d/1vCOIqZOaTzzKvLD7s_VDk8uxOwpg3u-hjIzy2z0D8sk/edit?usp=sharing",
+    contact: {
+      email: "olaopavictor@gmail.com",
+      website: "olaopa-victor-lendsqr-be-test.vercel.app"
+    }
+  });
+});
+
 app.use(authMiddleware); // Ensure this is applied before your routes
 app.use('/api', userRoutes);
 app.use('/api', checkBlacklistRoute);
